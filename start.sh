63f8ae86be1207a3f7f3774d2f870bc845c656c3
cblgh <cblgh@cblgh.org>
2019-05-22 21:35:41 +0200

    add start script for connecting to #cabal-club on freenode

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node index.js --key cabal://14bc77d788fdaf07b89b28e9d276e47f2e44011f4adb981921056e1b3b40e99e --cabalChannel default --cabalNick irc-bridge --ircServer irc.freenode.net --ircChannel \#cabal-club --ircNick cabal-bridge
