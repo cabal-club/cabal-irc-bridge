7e4ec92fb43eef73aaf91daf5f41456617410f73
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-05-21 20:09:15 -0400

    Refactor to use cabal-client instead of cabal-core

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 
-var Cabal = require('cabal-core')
-var swarm = require('cabal-core/swarm.js')
+var Client = require('cabal-client')
 var irc = require('irc')
 var minimist = require('minimist')
 var os = require("os")
-
-var homedir = os.homedir()
-var rootdir = homedir + `/.cabal/v${Cabal.databaseVersion}`
-var rootconfig = `${rootdir}/config.yml`
-var archivesdir = `${rootdir}/archives/`
 
 var args = minimist(process.argv.slice(2))
 
@@ -29,8 +23,7 @@ var usage = `Usage
     --cabalNick
 `
 
-var key = args.key.replace('cabal://', '').replace('cbl://', '').replace('dat://', '').replace(/\//g, '')
-var storage = archivesdir + key
+var key = (args.key || '').replace('cabal://', '').replace('cbl://', '').replace('dat://', '').replace(/\//g, '')
 
 var ircServer = args.ircServer
 var ircChannel = args.ircChannel
@@ -47,100 +40,68 @@ if (!key || !ircServer || !ircChannel) {
   process.exit(1)
 }
 
-console.log('➤ Cabal/IRC bridge started: ' + key.substr(0, 8) + '… with ' + ircChannel)
+var cabalClient = new Client()
+cabalClient.addCabal(key).then((cabalDetails) => {
+  cabalDetails.publishNick(cabalNick)
 
-var ircClient = new irc.Client(ircServer, ircNick, {
-  autoRejoin: true,
-  channels: [],
-  debug: !!ircDebug,
-  floodProtection: true,
-  password: ircPassword,
-  realName: 'Cabal/IRC message bridge bot',
-  retryCount: 5,
-  showErrors: true,
-  userName: ircUserName
-})
+  function sendCabalMessage (message) {
+    cabalDetails.publishMessage({
+      type: 'chat/text',
+      content: {
+        channel: cabalChannel,
+        text: message
+      }
+    })
+  }
 
-ircClient.addListener('error', function (message) {
-  console.log('==> IRC Error: ', message)
-})
+  function keyToNick (key) {
+    var user = cabalDetails.getUsers()[key]
+    if (user && user.name) {
+      return user.name
+    } else {
+      return key.substr(0, 5)
+    }
+  }
 
-var cabalUsers = {}
-var localUserKey
-
-var cabal = Cabal(storage, key)
-cabal.db.ready(() => {
-  swarm(cabal)
-
-  cabal.publishNick(cabalNick)
-
-  cabal.getLocalKey(function (err, lkey) {
-    if (err) return
-    localUserKey = lkey
-  })
-
-  cabal.messages.events.on(cabalChannel, function (message) {
+  const startTime = (new Date()).getTime()
+  cabalDetails.core.messages.events.on(cabalChannel, function (message) {
     if (message && message.value && message.value.content) {
       var text = message.value.content.text
-      // Omit relaying messages published by the bridge
-      if (message.key !== localUserKey) {
+      // Omit relaying old messages or messages published by the bridge
+      if (message.value.timestamp >= startTime && message.key !== cabalDetails.getLocalUser().key) {
         ircClient.say(ircChannel, irc.colors.wrap(irc.colors.codes.dark_green, '<' + keyToNick(message.key) + '> ') + text)
       }
     }
   })
 
-  cabal.users.getAll(function (err, users) {
-    if (err) return
-    cabalUsers = users
-
-    cabal.users.events.on('update', function (key) {
-      cabal.users.get(key, function (err, user) {
-        if (err) return
-        cabalUsers[key] = Object.assign(cabalUsers[key] || {}, user)
-      })
-    })
-    cabal.on('peer-added', function (key) {
-      var found = false
-      Object.keys(cabalUsers).forEach(function (k) {
-        if (k === key) {
-          found = true
-        }
-      })
-      if (!found) {
-        cabalUsers[key] = {
-          key: key
-        }
-      }
-    })
+  const ircClient = new irc.Client(ircServer, ircNick, {
+    autoRejoin: true,
+    channels: [],
+    debug: !!ircDebug,
+    floodProtection: true,
+    password: ircPassword,
+    realName: 'Cabal/IRC message bridge bot',
+    retryCount: 5,
+    showErrors: true,
+    userName: ircUserName,
+    autoConnect: false,
   })
 
-  ircClient.join(ircChannel, function () {
-    var startupMessage = '➤ Cabal/IRC bridge started'
+  ircClient.connect(() => {
+    ircClient.addListener('error', function (message) {
+      console.log('==> IRC Error: ', message)
+    })
+
+    ircClient.join(ircChannel, function () {
+      ircClient.addListener('message', function (from, to, message) {
+        message = '<' + from + '> ' + message
+        sendCabalMessage(message)
+      })
+    })
+
+    const startupMessage = '➤ Cabal/IRC bridge started: ' + key.substr(0, 8) + '… with ' + ircChannel;
+    console.log(startupMessage)
     ircClient.say(ircChannel, startupMessage)
     sendCabalMessage(startupMessage)
-
-    ircClient.addListener('message', function (from, to, message) {
-      message = '<' + from + '> ' + message
-      sendCabalMessage(message)
-    })
   })
 })
-
-function sendCabalMessage (message) {
-  cabal.publish({
-    type: 'chat/text',
-    content: {
-      channel: cabalChannel,
-      text: message
-    }
-  })
-}
-
-function keyToNick (key) {
-  var user = cabalUsers[key]
-  if (user && user.name) {
-    return user.name
-  } else {
-    return key.substr(0, 5)
-  }
-}
